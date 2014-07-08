6d0951851862c8973f8fd54f889d6664c95f0cbe
Gage Bachik <gbachik@gmail.com>
2014-07-07 22:28:14 -0600

    Waiting On Desc

--- main2.js
+++ main2.js
@@ -6,14 +6,43 @@ $(document).on('ready', function() {
 		$(packages[i]).fadeIn(750*(i+1));
 	};
 
-	if(location.search == "?show_div=1"){
-  		$('#OLD_DIV').hide();
-  		$('#NEW_DIV').show();
+	if(location.search === "?bronze"){
+		console.log('worked');
+		$('.buy-bronze').bPopup({
+			speed: 450,
+			transition: 'slideDown',
+		}); 
+	}	if(location.search === "?silver"){
+		console.log('worked');
+		$('.buy-silver').bPopup({
+			speed: 450,
+			transition: 'slideDown',
+		}); 
+	}	if(location.search === "?gold"){
+		console.log('worked');
+		$('.buy-gold').bPopup({
+			speed: 450,
+			transition: 'slideDown',
+		}); 
 	}
 
-	$(document).on('click', 'a', function(){
+	$(document).on('click', 'a', function(event){
+		event.preventDefault();
 		if ($(this).is('#buy-bronze')){
-			console.log('tet');
+			$('.buy-bronze').bPopup({
+				speed: 450,
+				transition: 'slideDown'
+			});   
+		} else if ($(this).is('#buy-silver')){
+			$('.buy-silver').bPopup({
+				speed: 450,
+				transition: 'slideDown',
+			}); 
+		} else if ($(this).is('#buy-gold')){
+			$('.buy-gold').bPopup({
+				speed: 450,
+				transition: 'slideDown',
+			}); 
 		}
 	});
 });
